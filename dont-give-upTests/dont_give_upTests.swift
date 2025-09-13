bd5c3d5a3b8477bd5a27e73d25e66e0db157aae2
Yves Courteau <yves.courteau@uqtr.ca>
2025-09-13 14:56:09 -0400

    Initial Commit

---
+++ dont-give-upTests/dont_give_upTests.swift
@@ -0,0 +1,17 @@
+//
+//  dont_give_upTests.swift
+//  dont-give-upTests
+//
+//  Created by Yves Courteau on 2025-09-13.
+//
+
+import Testing
+@testable import dont_give_up
+
+struct dont_give_upTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
